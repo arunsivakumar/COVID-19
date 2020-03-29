a8a3b5488b11a476c368bbfc57da7552447e51b2
Arun Sivakumar <arunsivakumar@live.com>
2020-03-30 05:27:32 +0800

    add global data to home

--- Covid-19/Views/Home/HomeViewController.swift
+++ Covid-19/Views/Home/HomeViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    var store: CovidStore!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var deathsLabel: UILabel!
+    @IBOutlet weak var recoveredLabel: UILabel!
+    @IBOutlet weak var activeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        loadData()
+    }
+    
+    private func setupUI() {
+        self.navigationItem.title = "Home"
+    }
+    
+    private func loadData() {
+        store.getGlobalData { [weak self] (result) in
+            guard let self = self else { return }
+            switch result {
+            case .success(let data): self.updateUI(data: data)
+            case .failure(let error): print(error)
+            }
+        }
+    }
+    
+    private func updateUI(data: GlobalDataDTO) {
+        totalLabel.text = data.cases.withCommas()
+        deathsLabel.text = data.deaths.withCommas()
+        recoveredLabel.text = data.recovered.withCommas()
+        activeLabel.text = data.active.withCommas()
     }
 }
-
