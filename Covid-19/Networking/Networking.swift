9da815378a9a2830d2e8cd67ff67008c5dfbf24d
Arun Sivakumar <arunsivakumar@live.com>
2020-03-30 05:26:21 +0800

    add networking

---
+++ Covid-19/Networking/Networking.swift
@@ -0,0 +1,78 @@
+//
+//  Networking.swift
+//  Covid-19
+//
+//  Created by Arun Sivakumar on 30/3/20.
+//  Copyright © 2020 Arun Sivakumar. All rights reserved.
+//
+
+import Foundation
+
+struct Networking {
+    
+    let urlSession: URLSession
+    
+    init(urlSession: URLSession) {
+        self.urlSession = urlSession
+    }
+}
+
+
+enum HttpMethod<Body> {
+    case get
+    case post(Body)
+}
+
+extension HttpMethod {
+    var method: String {
+        switch self {
+        case .get: return "GET"
+        case .post: return "POST"
+        }
+    }
+}
+
+struct Resource<A> {
+    var urlRequest: URLRequest
+    let parse: (Data) -> A?
+}
+
+extension Resource {
+    func map<B>(_ transform: @escaping (A) -> B) -> Resource<B> {
+        return Resource<B>(urlRequest: urlRequest) { self.parse($0).map(transform) }
+    }
+}
+
+extension Resource where A: Decodable {
+    init(get url: URL) {
+        self.urlRequest = URLRequest(url: url)
+        self.parse = { data in
+            let str = String(decoding: data, as: UTF8.self)
+            print("data: \(str)")
+            return try? JSONDecoder().decode(A.self, from: data)
+        }
+    }
+    
+    init<Body: Encodable>(url: URL, method: HttpMethod<Body>) {
+        urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method.method
+        switch method {
+        case .get: ()
+        case .post(let body):
+            self.urlRequest.httpBody = try! JSONEncoder().encode(body)
+        }
+        self.parse = { data in
+            try? JSONDecoder().decode(A.self, from: data)
+        }
+    }
+}
+
+extension URLSession {
+    func load<A>(_ resource: Resource<A>, completion: @escaping (A?) -> ()) {
+        dataTask(with: resource.urlRequest) { data, _, _ in
+            OperationQueue.main.addOperation {
+                completion(data.flatMap(resource.parse))
+            }
+        }.resume()
+    }
+}
