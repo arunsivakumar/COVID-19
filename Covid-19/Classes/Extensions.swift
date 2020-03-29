3543c9851f3b82eb2accceade1de439d862ecbc8
Arun Sivakumar <arunsivakumar@live.com>
2020-03-30 05:26:27 +0800

    add extension

---
+++ Covid-19/Classes/Extensions.swift
@@ -0,0 +1,39 @@
+//
+//  Extensions.swift
+//  Covid-19
+//
+//  Created by Arun Sivakumar on 30/3/20.
+//  Copyright © 2020 Arun Sivakumar. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+
+    class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            let moreNavigationController = tab.moreNavigationController
+
+            if let top = moreNavigationController.topViewController, top.view.window != nil {
+                return topViewController(base: top)
+            } else if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+}
+
+extension Int {
+    func withCommas() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        return numberFormatter.string(from: NSNumber(value:self))!
+    }
+}
