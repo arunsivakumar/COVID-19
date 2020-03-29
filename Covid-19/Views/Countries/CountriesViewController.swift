fb21165292ffb91c1cdbd82838ba5a2f536fb0ad
Arun Sivakumar <arunsivakumar@live.com>
2020-03-30 05:27:50 +0800

    add dashboard

---
+++ Covid-19/Views/Countries/CountriesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CountriesViewController.swift
+//  Covid-19
+//
+//  Created by Arun Sivakumar on 30/3/20.
+//  Copyright © 2020 Arun Sivakumar. All rights reserved.
+//
+
+import UIKit
+
+class CountriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.navigationItem.title = "Countries"
+    }
+
+}
